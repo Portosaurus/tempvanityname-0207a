3ef97b25f8d7f1648964a45a883a7087d2d0e6c1
PortosaurusBot <claperone.portasaurus@gmail.com>
2020-06-02 19:29:22 +0000

    Auto generated portfolio

---
+++ precache-manifest.669846da7d2091b9c42a7453b941e493.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61455b06e8a2e71833932be79e22f06e",
+    "url": "https://portosaurus.github.io/tempvanityname-0207a/index.html"
+  },
+  {
+    "revision": "1faca092173a5cffb728",
+    "url": "https://portosaurus.github.io/tempvanityname-0207a/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6bb64744ce24d0b410f1",
+    "url": "https://portosaurus.github.io/tempvanityname-0207a/static/js/2.18e4ac39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://portosaurus.github.io/tempvanityname-0207a/static/js/2.18e4ac39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1faca092173a5cffb728",
+    "url": "https://portosaurus.github.io/tempvanityname-0207a/static/js/main.e10559b9.chunk.js"
+  },
+  {
+    "revision": "1ad7d78afe9d7b516bfb",
+    "url": "https://portosaurus.github.io/tempvanityname-0207a/static/js/runtime-main.66d992ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://portosaurus.github.io/tempvanityname-0207a/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://portosaurus.github.io/tempvanityname-0207a/precache-manifest.669846da7d2091b9c42a7453b941e493.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://portosaurus.github.io/tempvanityname-0207a/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
